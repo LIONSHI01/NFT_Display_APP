5aea67ff573540a5385b004f795be10f9cdef4e8
LIONSHI01 <shizijian2010@yahoo.com.hk>
2022-05-28 11:59:32 +0800

    initial

---
+++ helper.js
@@ -0,0 +1,16 @@
+export const AJAX = async function (wallet) {
+  try {
+    const options = { method: "GET", headers: { Accept: "application/json" } };
+
+    const url = `https://api.opensea.io/api/v1/assets?owner=${wallet}&order_direction=desc&limit=100&include_orders=false`;
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    if (!response.ok) throw new Error(`${data.message} ${response.status}`);
+    console.log(data);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
---
+++ model.js
@@ -0,0 +1,16 @@
+import { AJAX } from "./helper";
+
+export const state = {
+  result: {
+    collection_name: "",
+    image_url: "",
+    token_id: "",
+  },
+};
+
+export const createSearchObject = function (data) {
+  const { assets } = data.assets;
+  console.log(assets);
+};
+
+console.log("test");
