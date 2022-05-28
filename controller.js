48104f26c5bce01c196b69764bf9492096e78014
LIONSHI01 <shizijian2010@yahoo.com.hk>
2022-05-28 16:34:45 +0800

    css modification

--- controller.js
+++ controller.js
@@ -1,6 +1,6 @@
 import { AJAX } from "./helper.js";
 // import * as model from "./model.js";
-const container = document.querySelector(".img-container");
+const imgContainer = document.querySelector(".img-container");
 const btnInput = document.querySelector(".inputBtn");
 const walletInput = document.querySelector(".input-wallet");
 
@@ -31,11 +31,8 @@ const generateMarkup = function (data) {
       <img class="nft-img" src="${el.image_url}"/>
         <ul>
           <li>${el.collection_name}</li>
-          <li>Token ID : #${el.token_id ? el.token_id : "No Token ID"}</li>
           <li>
-            <a class="nft-link" target="blank" href="${
-              el.permalink
-            }">Go to assets</a>
+            <a class="nft-link" target="blank" href="${el.permalink}">Go to assets</a>
           </li>
         </ul>
       </div>
@@ -46,7 +43,7 @@ const generateMarkup = function (data) {
 };
 
 const render = function (markup) {
-  container.insertAdjacentHTML("afterbegin", markup);
+  imgContainer.insertAdjacentHTML("beforeend", markup);
 };
 
 const startEngine = async function (wallet) {
@@ -65,3 +62,4 @@ btnInput.addEventListener("click", () => {
 const wallet1 = "0xA90c70882Fc63ac514bE15743a13595Cb39F767D";
 const wallet2 = "0x8742fa292AFfB6e5eA88168539217f2e132294f9";
 const wallet3 = "0x0449Bc01e1D8154A118c56aaA776272e94B45929";
+const wallet4 = "0x88B2b904147b94FD50F710D9Cbc5B8F07c982C30";
